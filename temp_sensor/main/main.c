c3517f5792ae225152d93a887d35e19d8284d44c
DuckKee <111925005+DuckKee@users.noreply.github.com>
2024-04-15 18:56:53 -0600

    Combined OLED and Sensors

--- temp_sensor/main/main.c
+++ temp_sensor/main/main.c
@@ -5,15 +5,60 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "sdkconfig.h"
+#include <stdio.h>
+
+#include "driver/i2c_master.h"
+#include "esp_err.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_log.h"
+#include "esp_lvgl_port.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "lvgl.h"
+
+#if CONFIG_EXAMPLE_LCD_CONTROLLER_SH1107
+#include "esp_lcd_sh1107.h"
+#else
+#include "esp_lcd_panel_vendor.h"
+#endif
+
+#define I2C_BUS_PORT 0
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////// Please update the following configuration according to your LCD spec //////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#define EXAMPLE_LCD_PIXEL_CLOCK_HZ (400 * 1000)
+#define EXAMPLE_PIN_NUM_SDA 3
+#define EXAMPLE_PIN_NUM_SCL 4
+#define EXAMPLE_PIN_NUM_RST -1
+#define EXAMPLE_I2C_HW_ADDR 0x3C
+
+// The pixel number in horizontal and vertical
+#if CONFIG_EXAMPLE_LCD_CONTROLLER_SSD1306
+#define EXAMPLE_LCD_H_RES 128
+#define EXAMPLE_LCD_V_RES 64
+#elif CONFIG_EXAMPLE_LCD_CONTROLLER_SH1107
+#define EXAMPLE_LCD_H_RES 64
+#define EXAMPLE_LCD_V_RES 128
+#endif
+// Bit number used to represent command and parameter
+#define EXAMPLE_LCD_CMD_BITS 8
+#define EXAMPLE_LCD_PARAM_BITS 8
+
+extern void example_lvgl_demo_ui(lv_disp_t *disp, char *message);
+
 
 static const char* TAG = "TPH";
+
+float temp, pres, hum;
 
 void app_main(void) {
     // Entry Point
     i2c_config_t i2c_cfg = {
         .mode = I2C_MODE_MASTER,
-        .sda_io_num = GPIO_NUM_21,
-        .scl_io_num = GPIO_NUM_22,
+        .sda_io_num = GPIO_NUM_10,
+        .scl_io_num = GPIO_NUM_11,
         .sda_pullup_en = false,
         .scl_pullup_en = false,
 
@@ -35,20 +80,116 @@ void app_main(void) {
     bmx280_config_t bmx_cfg = BMX280_DEFAULT_CONFIG;
     ESP_ERROR_CHECK(bmx280_configure(bmx280, &bmx_cfg));
 
-    DHT11_init(GPIO_NUM_4);
+    DHT11_init(GPIO_NUM_5);
 
-    while (1) {
-        ESP_ERROR_CHECK(bmx280_setMode(bmx280, BMX280_MODE_FORCE));
+    ESP_LOGI(TAG, "Initialize I2C bus");
+    i2c_master_bus_handle_t i2c_bus = NULL;
+    i2c_master_bus_config_t bus_config = {
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .glitch_ignore_cnt = 7,
+        .i2c_port = I2C_BUS_PORT,
+        .sda_io_num = EXAMPLE_PIN_NUM_SDA,
+        .scl_io_num = EXAMPLE_PIN_NUM_SCL,
+        .flags.enable_internal_pullup = true,
+    };
+    ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, &i2c_bus));
+
+    ESP_LOGI(TAG, "Install panel IO");
+    esp_lcd_panel_io_handle_t io_handle = NULL;
+    esp_lcd_panel_io_i2c_config_t io_config = {
+        .dev_addr = EXAMPLE_I2C_HW_ADDR,
+        .scl_speed_hz = EXAMPLE_LCD_PIXEL_CLOCK_HZ,
+        .control_phase_bytes = 1,                // According to SSD1306 datasheet
+        .lcd_cmd_bits = EXAMPLE_LCD_CMD_BITS,    // According to SSD1306 datasheet
+        .lcd_param_bits = EXAMPLE_LCD_CMD_BITS,  // According to SSD1306 datasheet
+#if CONFIG_EXAMPLE_LCD_CONTROLLER_SSD1306
+        .dc_bit_offset = 6,  // According to SSD1306 datasheet
+#elif CONFIG_EXAMPLE_LCD_CONTROLLER_SH1107
+        .dc_bit_offset = 0,  // According to SH1107 datasheet
+        .flags =
+            {
+                .disable_control_phase = 1,
+            }
+#endif
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c(i2c_bus, &io_config, &io_handle));
+
+    ESP_LOGI(TAG, "Install SSD1306 panel driver");
+    esp_lcd_panel_handle_t panel_handle = NULL;
+    esp_lcd_panel_dev_config_t panel_config = {
+        .bits_per_pixel = 1,
+        .reset_gpio_num = EXAMPLE_PIN_NUM_RST,
+    };
+#if CONFIG_EXAMPLE_LCD_CONTROLLER_SSD1306
+    ESP_ERROR_CHECK(esp_lcd_new_panel_ssd1306(io_handle, &panel_config, &panel_handle));
+#elif CONFIG_EXAMPLE_LCD_CONTROLLER_SH1107
+    ESP_ERROR_CHECK(esp_lcd_new_panel_sh1107(io_handle, &panel_config, &panel_handle));
+#endif
+
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
+
+#if CONFIG_EXAMPLE_LCD_CONTROLLER_SH1107
+    ESP_ERROR_CHECK(esp_lcd_panel_invert_color(panel_handle, true));
+#endif
+
+    ESP_LOGI(TAG, "Initialize LVGL");
+    const lvgl_port_cfg_t lvgl_cfg = ESP_LVGL_PORT_INIT_CONFIG();
+    lvgl_port_init(&lvgl_cfg);
+
+    const lvgl_port_display_cfg_t disp_cfg = {
+        .io_handle = io_handle,
+        .panel_handle = panel_handle,
+        .buffer_size = EXAMPLE_LCD_H_RES * EXAMPLE_LCD_V_RES,
+        .double_buffer = true,
+        .hres = EXAMPLE_LCD_H_RES,
+        .vres = EXAMPLE_LCD_V_RES,
+        .monochrome = true,
+        .rotation = {
+            .swap_xy = false,
+            .mirror_x = false,
+            .mirror_y = false,
+        }};
+    lv_disp_t *disp = lvgl_port_add_disp(&disp_cfg);
+
+    /* Rotation of the screen */
+    lv_disp_set_rotation(disp, LV_DISP_ROT_NONE);
+
+        if (lvgl_port_lock(0)) {
+            while(1) {
+                        ESP_ERROR_CHECK(bmx280_setMode(bmx280, BMX280_MODE_FORCE));
         do {
             vTaskDelay(pdMS_TO_TICKS(1));
         } while (bmx280_isSampling(bmx280));
 
-        float temp = 0, pres = 0, hum = 0;
+        temp = 0, pres = 0, hum = 0;
         ESP_ERROR_CHECK(bmx280_readoutFloat(bmx280, &temp, &pres, &hum));
         hum = DHT11_read().humidity;
         ESP_LOGI(TAG, "Read Values: temp = %f, pres = %f, hum = %f", temp, pres, hum);
         ESP_LOGI(TAG, "Status code is %d", DHT11_read().status);
         vTaskDelay(2000 / portTICK_PERIOD_MS);  // DHT11 should be read at intervals of more than 1 second
-       
-    }
+        
+        ESP_LOGI(TAG, "Display LVGL Scroll Text");
+        size_t i = 0;
+
+        char tempResult[100], humResult[100], presResult[100];
+        sprintf(tempResult, "%f", temp);
+        sprintf(humResult, "%f", hum);
+        sprintf(presResult, "%f", pres);
+
+        char *messages[] = {
+            tempResult,
+            humResult,
+            presResult
+        };
+                example_lvgl_demo_ui(disp, messages[i]);
+                i = (i + 1) % 3;
+                vTaskDelay(10000 / portTICK_PERIOD_MS);
+                // vTaskDelay(pdMS_TO_TICKS(5000));
+                // Release the mutex
+                lvgl_port_unlock();
+                break;
+            }
+        }
 }
